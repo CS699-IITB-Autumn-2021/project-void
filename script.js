cd5ec035f13c50d49c4fab0d7da5b7772d158cbc
rajasekhar2325 <rajasekhar2325@gmail.com>
2021-10-13 09:36:05 +0530

    updated label list and export button

--- script.js
+++ script.js
@@ -8,7 +8,28 @@ function ExpandImg(img) {
     expandimg.src = img.src;
     expandimg.title = img.title;
     expandimg.parentElement.style.display = "block";
+    displaylabels(img);
+
 }
+
+function drawlabelrect(x1, y1, x2, y2) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
+    ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 3;
+}
+
+function displaylabels(img) {
+    var labellist = document.getElementById("label");
+    labellist.innerHTML = "";
+    for (var i = 0; i < results[img.title].length; i++) {
+        var div = document.createElement("div");
+        var coord = results[img.title][i]["x1"] + "," + results[img.title][i]["y1"] + "," + results[img.title][i]["x2"] + "," + results[img.title][i]["y2"];
+        div.innerHTML = "<label onclick=\"drawlabelrect(" + coord + ")\"style=\"border:#099EA7 2px solid\";>" + results[img.title][i]["label"] + "</label>";
+        labellist.insertBefore(div, null);
+    }
+}
+
 
 /*To slide to next image on the main window when a next button is clicked*/
 /*these buttons follow a circular loop on thumbnail images*/
@@ -17,6 +38,8 @@ function nextimg() {
     var index = filelist.indexOf(currimg.title);
     currimg.src = img_src[filelist[(index + 1) % (filelist.length)]][0];
     currimg.title = filelist[(index + 1) % (filelist.length)];
+    displaylabels(currimg);
+
 }
 /*To slide to previous image on the main window when a previous button is clicked*/
 function previmg() {
@@ -26,13 +49,15 @@ function previmg() {
         index = filelist.length;
     currimg.src = img_src[filelist[(index - 1)]][0];
     currimg.title = filelist[(index - 1)];
+    displaylabels(currimg);
+
 }
 
 /*To download the output json file with image labels*/
 //needs to be updated
 function exportjson() {
     const filename = 'data.json';
-    const jsonStr = JSON.stringify(filelist);
+    const jsonStr = JSON.stringify(results);
     let element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonStr));
     element.setAttribute('download', filename);
@@ -49,23 +74,25 @@ function exportjson() {
 var img_src = {};
 // filelist array stores the image titles in the order of the thumbnails
 var filelist = [];
-
+var results = {};
+var canvas;
+var ctx;
 $(document).ready(function () {
 
     //global variables
-    let canvas = document.getElementById('canvas');
+    canvas = document.getElementById('canvas');
     // let pic = document.getElementById('expimg');
     let input_box = document.getElementById('input-box');
     let input_ele=document.getElementById('input-ele');
-    let ctx=canvas.getContext('2d');
+    ctx=canvas.getContext('2d');
     
     // let canvasx = canvas.offsetLeft;
     // let canvasy = canvas.offsetTop;
     let last_mousex = last_mousey = 0;
     let mousex = mousey = 0;
     let mousedown = false;
-
-    let results= {'img':"",'bounding_boxes':[]};
+    var pic;
+    //  results= {'img':"",'bounding_boxes':[]};
 
 
 
@@ -94,8 +121,8 @@ $(document).ready(function () {
                     output.insertBefore(div, null);
                     img_src[f.name] = temp;
                     filelist.push(f.name);
-
-                    results['img']= f.name
+                    results[f.name] = new Array();
+                    
                 });
                 fileReader.readAsDataURL(f);
 
@@ -109,7 +136,7 @@ $(document).ready(function () {
 
     function imageresize(){
         $('#expimg').on('load',function(){
-            let pic = document.getElementById('expimg');
+            pic = document.getElementById('expimg');
 
             image_width=pic.clientWidth;
             image_height=pic.clientHeight;
@@ -143,8 +170,9 @@ $(document).ready(function () {
             let input_value=document.getElementById('input-ele').value
             
 
-            let temp_data={ 'x1':last_mousex, 'y1':last_mousey,'x2':mousex, 'y2':mousey, 'label':input_value}
-            results['bounding_boxes'].push(temp_data)
+            let temp_data = { 'x1': last_mousex, 'y1': last_mousey, 'x2': mousex, 'y2': mousey, 'label': input_value }
+            results[pic.title].push(temp_data)
+            displaylabels(pic);
            
             console.log(results);
         }
@@ -179,11 +207,11 @@ $(document).ready(function () {
             let height = mousey-last_mousey;
             ctx.rect(last_mousex,last_mousey,width,height);
             ctx.strokeStyle = 'black';
-            ctx.lineWidth = 2;
+            ctx.lineWidth = 3;
             ctx.stroke();
         }
         //Output
-        document.getElementById('output').innerHTML='current: '+mousex+', '+mousey+'<br/>last: '+last_mousex+', '+last_mousey+'<br/>mousedown: '+mousedown;
+        // document.getElementById('output').innerHTML='current: '+mousex+', '+mousey+'<br/>last: '+last_mousex+', '+last_mousey+'<br/>mousedown: '+mousedown;
     
     });
 
@@ -198,6 +226,5 @@ $(document).ready(function () {
 
 
 });
-
 
 
